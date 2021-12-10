6eb5cf42798edbe711470e7c6c6295d03a770b3f
Alan Jones <jones.alan@hotmail.co.uk>
2021-12-10 17:45:25 +0100

    update

--- sqlpandas/sqlpandas.py
+++ sqlpandas/sqlpandas.py
@@ -2,12 +2,12 @@ import pandas as pd
 import sqlite3 as sql
 
 # create df
-election_df = pd.read_csv("https://researchbriefings.files.parliament.uk/documents/CBP-8749/HoC-GE2019-results-by-constituency-csv.csv")
-
-print (election_df)
+election_df = pd.read_csv('elections.csv')
 
 # create db
 conn = sql.connect('elections.db')
-election_df.to_sql('elections', conn)
 
-election_df.to_csv('elections.csv')
+cur = conn.execute("SELECT * FROM elections")
+rows = cur.fetchall()
+print(rows[0:4])
+
