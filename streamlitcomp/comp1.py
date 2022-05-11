85a5f59e608d808180f0eaa9fc9af57c540a69fc
Alan Jones <jones.alan@hotmail.co.uk>
2022-05-11 12:01:16 +0200

    update

--- streamlitcomp/comp1.py
+++ streamlitcomp/comp1.py
@@ -1,29 +1,38 @@
 import streamlit as st
 import streamlit.components.v1 as components
 
+def bs():
+    cdn = """
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    """
+    return cdn
+
 def header(text,c,s=1):
 
     html = f"""
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-
+        {bs()}
         <h{s} style='color:{c}'>{text}<h{s}>"""
-    return  st.components.v1.html(html)
+    return  components.html(html)
 
 def alert(text,type='alert-primary'):
     html = f"""
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-
+    {bs()}
     <div class="alert {type}" role="alert">
         {text}
     </div>
-    
     """
-    return  st.components.v1.html(html)
+
+    return  components.html(html)
 
 
-header("hello",'green',2)
+
+header("hello","green",2)
 
 alert("alert!!!!","alert-secondary")
+
+
+
+
 
 
 
