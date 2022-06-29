0efc6cd7d95d4015ae7202674643a22fb2d7617c
Alan Jones <jones.alan@hotmail.co.uk>
2022-06-29 17:33:55 +0200

    first commit

---
+++ pyscript-flask/README.md
@@ -0,0 +1,12 @@
+# PyScript v. Flask
+The code in this folder supports the Medium article []()
+
+## PyScript allows us to create a serverless web application with HTML and Python as the scripting language.
+
+
+
+
+See other articles on my [Github site](https://alanjones2.github.io)
+
+If you find this content useful, please consider this... <br/><br/>
+<a href='https://ko-fi.com/M4M64THKG' target='_blank'><img height='36' style='border:0px;height:36px;' src='https://cdn.ko-fi.com/cdn/kofi2.png?v=2' border='0' alt='Buy Me a Coffee at ko-fi.com' /></a>
---
+++ pyscript-flask/flask/app.py
@@ -0,0 +1,28 @@
+from flask import Flask, config, render_template, request
+import pandas as pd
+import json
+import plotly
+import plotly.express as px
+
+app = Flask(__name__)
+   
+@app.route('/')
+def index():
+    return render_template('index.html',  graphJSON=getGraph())
+
+@app.route('/callback', methods=['POST', 'GET'])
+def cb():
+    return getGraph(request.args.get('data'))
+
+def getGraph(chart="Tmax"):
+    print(chart)
+    url = 'https://raw.githubusercontent.com/alanjones2/uk-historical-weather/main/data/Heathrow.csv'
+    
+    df = pd.read_csv(url)
+    df = df[df['Year']==2020]
+
+    fig = px.line(df,
+    x="Month", y=chart,
+    width=800, height=400)
+    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+    return graphJSON
