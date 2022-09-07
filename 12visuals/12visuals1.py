3b38fc3b5cce89f063b89e4bff1a420c8dfd6449
Alan Jones <jones.alan@hotmail.co.uk>
2022-09-07 14:23:09 +0200

    update stgrid

--- 12visuals/12visuals1.py
+++ 12visuals/12visuals1.py
@@ -9,7 +9,7 @@ caption2 = "That's 5º up from yesterday"
 
 fig, ax = plt.subplots()
 plt.bar(days,temps)
-plt.suptitle(caption)
+plt.suptitle(caption, fontweight = 'bold')
 plt.title(caption2)
 st.pyplot(fig)
 
@@ -33,7 +33,7 @@ fig2, ax2 = plt.subplots(figsize=(5,1))
 
 ax2.text(0, 0.9, temps[1],
         verticalalignment='top', horizontalalignment='left',
-        color='red', fontsize=18)
+        color='red', fontsize=18, fontweight = 'bold')
 ax2.text(0.2, 0.9, caption,
         verticalalignment='top', horizontalalignment='left',
         color='Black', fontsize=10)
@@ -48,6 +48,27 @@ ax2.spines['top'].set_visible(False)
 plt.xticks([])
 plt.yticks([])
 
-
-
 st.pyplot(fig2)
+
+st.markdown("---")
+
+fig3, ax3 = plt.subplots(figsize=(4,2))
+
+ax3.text(0.5, 0.9, temps[1],
+        verticalalignment='top', horizontalalignment='center',
+        color='darkblue', fontsize=64, fontweight = 'bold')
+ax3.text(0.5, 0.3, caption,
+        verticalalignment='top', horizontalalignment='center',
+        color='blue', fontsize=8)
+ax3.text(0.5, 0.2, caption2,
+        verticalalignment='top', horizontalalignment='center',
+        color='lightblue', fontsize=6)
+
+#ax3.spines['right'].set_visible(False)
+#ax3.spines['left'].set_visible(False)
+#ax3.spines['bottom'].set_visible(False)
+#ax3.spines['top'].set_visible(False)
+plt.xticks([])
+plt.yticks([])
+
+st.pyplot(fig3)
---
+++ 12visuals/grid1.py
@@ -0,0 +1,54 @@
+import streamlit as st
+
+# 2x2 grid the long way
+
+with st.container():
+    col1, col2 = st.columns(2)
+    with col1:
+        st.write('Caption for first chart')
+    with col2:
+        st.line_chart((0,1), height=100)
+
+with st.container():
+    col1, col2 = st.columns(2)
+    with col1:
+        st.write('Caption for second chart')
+    with col2:
+        st.line_chart((1,0), height=100)
+
+st.markdown('---')
+
+# 2x2 grid using an array
+
+grid = [[],[]]
+
+with st.container():
+    grid[0] = st.columns(2)
+with st.container():
+    grid[1] = st.columns(2)
+
+grid[0][0].write('Caption for first chart')
+grid[0][1].line_chart((0,1), height=100)
+grid[1][0].write('Caption for second chart')
+grid[1][1].line_chart((1,0), height=100)
+
+st.markdown('---')
+
+# make any grid with a function
+
+def make_grid(cols,rows):
+    grid = [0 for i in range(cols)]
+    for i in range(cols):
+        with st.container():
+            grid[i] = st.columns(rows)
+    return grid
+        
+
+mygrid = make_grid(5,5)
+
+mygrid[0][0].write('00')
+mygrid[1][1].write('11')
+mygrid[2][2].write('22')
+mygrid[3][3].write('33')
+mygrid[4][4].write('44')
+
