4fb0e8617551690c176a6fac84a6e37293aa83ab
Alan Jones <jones.alan@hotmail.co.uk>
2022-09-24 17:32:33 +0200

    update

--- 12visuals/12visuals3.py
+++ 12visuals/12visuals3.py
@@ -3,16 +3,18 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 
-url='https://raw.githubusercontent.com/alanjones2/dataviz/master/tempbydecade.csv'
-gwdec = pd.read_csv(url, delimiter=' ', skipinitialspace=True)
+url='data/bydecade.csv'
+gwdec = pd.read_csv(url)
 gwdec=gwdec.set_index('Year')
 st.table(gwdec)
 
 fig, ax = plt.subplots()
-
 ax.imshow(gwdec)
-
 ax.set_xticks(range(len(gwdec.columns)))
-
 ax.set_xticklabels(gwdec.columns, rotation=45)
+st.pyplot(fig)
+
+import seaborn as sns
+fig, ax = plt.subplots()
+sns.heatmap(gwdec)
 st.pyplot(fig)
