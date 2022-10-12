8a6bef7680da176c6ef82bf3309817776e3f9dc3
Alan Jones <jones.alan@hotmail.co.uk>
2022-10-12 15:15:40 +0200

    remove 12 visuals, add infographic

--- 12visuals/data.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-
-url='data/tempbydecade.csv'
-gwdec = pd.read_csv(url, delimiter=' ', skipinitialspace=True)
-gwdec=gwdec.set_index('Year')
-gwdec.to_csv('data/bydecade.csv')
