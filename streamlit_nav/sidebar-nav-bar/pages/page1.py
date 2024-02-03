563971e26e5b79565d4b37155c9c79c7ecf60f7b
Alan Jones <jones.alan@hotmail.co.uk>
2024-02-03 18:13:03 +0100

    update

--- streamlit_nav/sidebar-nav-bar/pages/page1.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-import navbar
-
-current_page = "Page 1"
-st.header(current_page)
-
-with st.sidebar: navbar.nav(current_page)
--- streamlit_nav/sidebar-nav-bar/pages/page2.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-import navbar
-
-current_page = "Page 2"
-st.header(current_page)
-
-with st.sidebar:navbar.nav2(current_page)
